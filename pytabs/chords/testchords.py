08aeaf0a02480fe58b39d3f8a2a205beb3476c9f
Milos Simic <milossimicsimo@gmail.com>
2014-12-17 18:53:08 +0100

    [NEW] GuitarChordProcesor [UPDATE] gramatika za akorde promenjeno sa chords->values da bi se postigla genericnost sa ostalim gramatikama

--- pytabs/chords/testchords.py
+++ pytabs/chords/testchords.py
@@ -3,10 +3,6 @@ Created on Nov 27, 2014
 
 @author: Milos
 '''
-from types import NoneType
-
-from textx.metamodel import metamodel_from_file
-
 """
     Procesor koji ce u slucaju da se pojavi akord bez pridruzene INT vrednosti, 
     prebaciti 0 u prazan string. Na ovaj nacin mingus moze da svira samo akord.
@@ -14,6 +10,12 @@ from textx.metamodel import metamodel_from_file
     Args:move_cmd model koji se proverava
     
 """
+import os
+from types import NoneType
+
+from textx.metamodel import metamodel_from_file
+
+
 def move_command_processor(move_cmd):
     if move_cmd.number == 0:
         move_cmd.number = ""
@@ -67,10 +69,9 @@ class Music:
         return suffixret
     
     def interpret(self, model):
-        
         chords = []
         
-        for c in model.chords:
+        for c in model.value:
             if(c.__class__.__name__=="ChordExtended"):
                 if(c.suffix):
                     #ima povisilicu ili snizilicu
@@ -135,7 +136,40 @@ class Music:
                 print("pause {}".format(c.time))
 
 
+GRAMMAR_PATH = os.path.dirname(os.path.realpath(__file__)) + "/grammar/"
 
+class GuitarChordProcessor(object):
+    def __init__(self, guitar_chords_grammar_file=None):
+        
+        if not guitar_chords_grammar_file:
+            guitar_chords_grammar_file = GRAMMAR_PATH + "chords.tx"
+        
+        self.guitar_chords_mm = metamodel_from_file(guitar_chords_grammar_file, debug=False)
+        self.guitar_chords_mm.register_obj_processors({'BaseExtended': self.chord_command_processor,
+                                      "PrepExtended":move_command_processor,
+                                      "DecorateExtended":move_command_processor})
+        #guitar_chords_model = guitar_chords_mm.model_from_file('examples/rythm.mcx')
+        
+    
+    def chord_command_processor(self,move_cmd):
+        """
+        Procesor koji ce u slucaju da se pojavi akord bez pridruzene INT vrednosti, 
+        prebaciti 0 u prazan string. Na ovaj nacin mingus moze da svira samo akord.
+        
+        Args:move_cmd model koji se proverava
+        
+        """
+        if move_cmd.number == 0:
+            move_cmd.number = ""
+            
+    def guitarchords_model_from_file(self, file_path):
+        #'examples/rythm.mcx'
+        self.guitar_chords_model = self.guitar_chords_mm.model_from_file(file_path)
+        
+    def guitarmodel_interprete(self):
+        music = Music()
+        music.interpret(self.guitar_chords_model)
+    
 if __name__ == "__main__":
     robot_mm = metamodel_from_file('grammer/chords.tx', debug=False)
     robot_mm.register_obj_processors({'BaseExtended': move_command_processor,
--- pytabs/chords/testsong.py
+++ pytabs/chords/testsong.py
@@ -5,6 +5,8 @@ Created on Dec 13, 2014
 '''
 from textx.metamodel import metamodel_from_file
 
+from pytabs.chords.testchords import Music
+
 
 class Song:
     def interpret(self, model):
@@ -13,8 +15,10 @@ class Song:
                                             model.header.tempo))
         for seq in model.sequences:
             print("{} {}".format(seq.type,seq.name))
-            for val in seq.value.chords:
-                print val.prefix.chord.base
+            """for val in seq.value.chords:
+                print val.prefix.chord.base"""
+            music = Music()
+            music.interpret(seq.value)
 
 if __name__ == "__main__":
     robot_mm = metamodel_from_file('grammer/composition.tx', debug=False)
